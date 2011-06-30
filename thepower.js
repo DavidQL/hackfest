f052149d6592e5bd88c09ca05322c309fc507c8d
David Aragon <david@quickleft.com>
2011-06-29 18:40:53 -0600

    initial commit

--- thepower.js
+++ thepower.js
@@ -1,36 +1,15 @@
-/**
- *  _   _    _    ____ _  _______ _____ ____ _____ 
- * | | | |  / \  / ___| |/ /  ___| ____/ ___|_   _|
- * | |_| | / _ \| |   | ' /| |_  |  _| \___ \ | |  
- * |  _  |/ ___ \ |___| . \|  _| | |___ ___) || |  
- * |_| |_/_/   \_\____|_|\_\_|   |_____|____/ |_|  
- *
- * QuickLeft Hackfest
- * April 27 2011
- *
- * JavaScript
- * Written by, Nico Valencia
- *
- * @module HACKFEST
- */
-
-(function( window, undefined ){
-
-  var HACKFEST = {
-
-    init: function () {
-
-      // Begin your mad JavaScript in here!
-
-    },
-
-    anotherFunction: function () {
-
-
+$(document).ready(function() {
+  $('input#search').keyup(function(e) {
+    var query;
+    if (e.keyCode === 13) {
+      query = $(this).val();
+      $.getJSON("http://search.twitter.com/search.json?q="+query+"&rpp=100&callback=?", function(data) { 
+        _.each(data.results, function(tweet) {
+          if (tweet.geo !== null) {
+            console.log(tweet.geo);
+          }
+        });
+      });
     }
-
-  };
-
-  $( document ).ready( HACKFEST.init );
-
-})( this );
+  });
+});
